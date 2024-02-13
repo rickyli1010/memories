f1df86a024156c34d93f673598d8471b8d9277a7
rickyli1010 <ricky_0321@hotmail.com>
2024-02-13 15:17:21 -0600

    auto route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import 'dotenv/config';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('user', userRoutes);
+
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
