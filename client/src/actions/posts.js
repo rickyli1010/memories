20ed63d501f73842a115c180a5235b09d972f1b7
rickyli1010 <ricky_0321@hotmail.com>
2024-02-20 21:39:55 -0600

    add loading screen

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -4,15 +4,19 @@ import {
   UPDATE,
   DELETE,
   LIKE,
-  FETCH_BY_SEARCH
+  FETCH_BY_SEARCH,
+  START_LOADING,
+  END_LOADING
 } from '../constants/actionTypes';
 import * as api from '../api';
 
 // Action Creators
 export const getPosts = (page) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
     const { data } = await api.fetchPosts(page);
     dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error.message);
   }
@@ -20,10 +24,12 @@ export const getPosts = (page) => async (dispatch) => {
 
 export const getPostBySearch = (searchQuery) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
     const {
       data: { data }
     } = await api.fetchPostsBySearch(searchQuery);
     dispatch({ type: FETCH_BY_SEARCH, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
   }
@@ -31,8 +37,10 @@ export const getPostBySearch = (searchQuery) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
     const { data } = await api.createPost(post);
     dispatch({ type: CREATE, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
   }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,11 +4,17 @@ import {
   UPDATE,
   DELETE,
   LIKE,
-  FETCH_BY_SEARCH
+  FETCH_BY_SEARCH,
+  START_LOADING,
+  END_LOADING
 } from '../constants/actionTypes';
 
-export default (state = [], action) => {
+export default (state = { isLoading: true, posts: [] }, action) => {
   switch (action.type) {
+    case START_LOADING:
+      return { ...state, isLoading: true };
+    case END_LOADING:
+      return { ...state, isLoading: false };
     case FETCH_ALL:
       return {
         ...state,
@@ -18,16 +24,27 @@ export default (state = [], action) => {
       };
     case FETCH_BY_SEARCH:
       return { ...state, posts: action.payload };
-    case DELETE:
-      return state.filter((post) => post._id !== action.payload);
-    case UPDATE:
     case LIKE:
-      return state.map((post) =>
-        post._id === action.payload._id ? action.payload : post
-      );
-
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        )
+      };
     case CREATE:
-      return [...state, action.payload];
+      return { ...state, posts: [...state.posts, action.payload] };
+    case UPDATE:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        )
+      };
+    case DELETE:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== action.payload)
+      };
     default:
       return state;
   }
